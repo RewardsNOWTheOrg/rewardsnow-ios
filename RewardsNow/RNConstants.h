336ae98805f970bd4b38852e38e458b80e5c3e15
Ethan Mick <ethan@cloudmine.me>
2013-04-05 16:17:52 -0400

    Added Constants File.

---
+++ RewardsNow/RNConstants.h
@@ -0,0 +1,13 @@
+//
+//  RNConstants.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/5/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNConstants : NSCache
+
+@end
