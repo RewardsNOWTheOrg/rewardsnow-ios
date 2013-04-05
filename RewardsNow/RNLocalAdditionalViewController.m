121de0eed6a656f68e8c5f552cf8b70b2645dbe6
Ethan Mick <ethan@cloudmine.me>
2013-04-05 16:46:07 -0400

    Added local deal view additional info.

---
+++ RewardsNow/RNLocalAdditionalViewController.m
@@ -0,0 +1,38 @@
+//
+//  RNLocalAdditionalViewController.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/5/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNLocalAdditionalViewController.h"
+
+@interface RNLocalAdditionalViewController ()
+
+@end
+
+@implementation RNLocalAdditionalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
