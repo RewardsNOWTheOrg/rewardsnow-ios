91b7168292ba85cc49122953b2940e6e2f4a930b
Ethan Mick <ethan@cloudmine.me>
2013-04-18 16:58:49 -0400

    Added AUthPasswordViewController

---
+++ RewardsNow/RNAuthPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNAuthPasswordViewController.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/18/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAuthPasswordViewController : UIViewController
+
+@end
---
+++ RewardsNow/RNAuthPasswordViewController.m
@@ -0,0 +1,38 @@
+//
+//  RNAuthPasswordViewController.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/18/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNAuthPasswordViewController.h"
+
+@interface RNAuthPasswordViewController ()
+
+@end
+
+@implementation RNAuthPasswordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
