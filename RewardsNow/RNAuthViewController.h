fb5a69a2b160a62253cf674237a2e27773c92130
Ethan Mick <ethan@cloudmine.me>
2013-04-18 14:51:20 -0400

    Added an Authentication view.

---
+++ RewardsNow/RNAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNAuthViewController.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/18/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAuthViewController : UIViewController
+
+@end
