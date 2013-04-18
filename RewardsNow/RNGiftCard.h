ee627401db772abfc934193137948e2e3cc5c8d9
Ethan Mick <ethan@cloudmine.me>
2013-04-18 18:18:30 -0400

    Added fields and transformation.

--- RewardsNow/RNGiftCard.h
+++ RewardsNow/RNGiftCard.h
@@ -8,6 +8,15 @@
 
 #import "RNObject.h"
 
+/*
+ 
+ "Description": "$25 JC Penney eGift Card",
+ "GiftCardNumber": "6006492001232167308"
+ */
+
 @interface RNGiftCard : RNObject
+
+@property (nonatomic, copy) NSString *cardDescription;
+@property (nonatomic, strong) NSNumber *cardNumber;
 
 @end
--- RewardsNow/RNGiftCard.m
+++ RewardsNow/RNGiftCard.m
@@ -10,4 +10,18 @@
 
 @implementation RNGiftCard
 
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    return @{@"cardDescription": @"Description",
+             @"cardNumber" : @"GiftCardNumber",
+             };
+}
+
++ (NSValueTransformer *)stateJSONTransformer {
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:^(NSString *str) {
+        return [NSNumber numberWithInteger:[str integerValue]];
+    } reverseBlock:^(NSNumber *state) {
+        return state;
+    }];
+}
+
 @end
