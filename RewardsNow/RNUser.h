bdc59a37112b07681a39b2bbaf4f408f18966e2e
Ethan Mick <ethan@cloudmine.me>
2013-04-18 15:24:22 -0400

    Added User object.

---
+++ RewardsNow/RNUser.h
@@ -0,0 +1,13 @@
+//
+//  RNUser.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/18/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNUser : NSObject
+
+@end
---
+++ RewardsNow/RNUser.m
@@ -0,0 +1,13 @@
+//
+//  RNUser.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/18/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNUser.h"
+
+@implementation RNUser
+
+@end
