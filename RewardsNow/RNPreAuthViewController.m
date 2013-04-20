f1c1fc823b20edf9a911565dde15b634a2e70081
Ethan Mick <ethan@cloudmine.me>
2013-04-20 14:51:45 -0400

    Adding PreAuthViewController

---
+++ RewardsNow/RNPreAuthViewController.m
@@ -0,0 +1,38 @@
+//
+//  RNPreAuthViewController.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/20/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNPreAuthViewController.h"
+
+@interface RNPreAuthViewController ()
+
+@end
+
+@implementation RNPreAuthViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
