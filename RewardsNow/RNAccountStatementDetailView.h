169bfdc2f74082bc667aa21a85b0462f193ffc13
Ethan Mick <ethan@cloudmine.me>
2013-04-23 10:49:32 -0400

    Adding the account statement detail view.

---
+++ RewardsNow/RNAccountStatementDetailView.h
@@ -0,0 +1,13 @@
+//
+//  RNAccountStatementDetailView.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 4/23/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAccountStatementDetailView : UIView
+
+@end
