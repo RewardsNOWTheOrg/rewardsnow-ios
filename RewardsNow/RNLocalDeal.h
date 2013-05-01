490df07790c5a5616589f3c198bf21a8b440ac12
Ethan Mick <ethan@cloudmine.me>
2013-05-01 17:10:27 -0400

    First pass at creating the object.

--- RewardsNow/RNLocalDeal.h
+++ RewardsNow/RNLocalDeal.h
@@ -7,7 +7,28 @@
 //
 
 #import "RNObject.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface RNLocalDeal : RNObject
+
+@property (nonatomic, strong) NSDate *activeFrom;
+@property (nonatomic, strong) NSDate *activeTo;
+@property (nonatomic, copy) NSString *additionalInformation;
+@property (nonatomic, copy) NSString *address;
+@property (nonatomic, copy) NSString *businessName;
+@property (nonatomic, copy) NSString *country;
+@property (nonatomic, copy) NSString *localDealDescription;
+@property (nonatomic, copy) NSString *discountString;
+@property (nonatomic, strong) NSNumber *discountValue;
+@property (nonatomic, strong) NSNumber *offerID;
+@property (nonatomic, strong) NSURL *imageURL;
+@property (nonatomic, strong) CLLocation *location;
+@property (nonatomic, strong) NSNumber *latitude;
+@property (nonatomic, strong) NSNumber *longitude;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *phoneNumber;
+@property (nonatomic, copy) NSString *state;
+@property (nonatomic, copy) NSString *website;
+@property (nonatomic, copy) NSString *zipCode;
 
 @end
--- RewardsNow/RNLocalDeal.m
+++ RewardsNow/RNLocalDeal.m
@@ -10,4 +10,45 @@
 
 @implementation RNLocalDeal
 
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    
+    NSMutableDictionary *fields = [NSMutableDictionary dictionaryWithDictionary:[super JSONKeyPathsByPropertyKey]];
+    [fields setValue:@"ActiveFrom" forKey:@"activeFrom"];
+    [fields setValue:@"ActiveTo" forKey:@"activeTo"];
+    [fields setValue:@"AdditionalInfo" forKey:@"additionalInformation"];
+    [fields setValue:@"Address" forKey:@"address"];
+    [fields setValue:@"BusinessName" forKey:@"businessName"];
+    [fields setValue:@"Country" forKey:@"country"];
+    [fields setValue:@"Description" forKey:@"localDealDescription"];
+    [fields setValue:@"Discount" forKey:@"discountString"];
+    [fields setValue:@"DiscountValue" forKey:@"discountValue"];
+    [fields setValue:@"IdOffer" forKey:@"offerID"];
+    [fields setValue:@"Image" forKey:@"imageURL"];
+    [fields setValue:@"Latitude" forKey:@"latitude"];
+    [fields setValue:@"Longitude" forKey:@"longitude"];
+    [fields setValue:@"Name" forKey:@"name"];
+    [fields setValue:@"Phone" forKey:@"phoneNumber"];
+    [fields setValue:@"State" forKey:@"state"];
+    [fields setValue:@"WebSite" forKey:@"website"];
+    [fields setValue:@"ZipCode" forKey:@"zipCode"];
+    return fields;
+}
+
++ (NSDateFormatter *)dateFormatter {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    return dateFormatter;
+}
+
++ (NSValueTransformer *)imageURLJSONTransformer {
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
+- (CLLocation *)location {
+    if (_location == nil) {
+        _location = [[CLLocation alloc] initWithLatitude:self.latitude.doubleValue longitude:self.longitude.doubleValue];
+    }
+    return _location;
+}
+
 @end
