59bf48679244cd5765bb18acbbce8fcacc316193
Ethan Mick <ethan@cloudmine.me>
2013-05-01 16:48:09 -0400

    Added Local Deal object.

---
+++ RewardsNow/RNLocalDeal.h
@@ -0,0 +1,13 @@
+//
+//  RNLocalDeal.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/1/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNObject.h"
+
+@interface RNLocalDeal : RNObject
+
+@end
---
+++ RewardsNow/RNLocalDeal.m
@@ -0,0 +1,13 @@
+//
+//  RNLocalDeal.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/1/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNLocalDeal.h"
+
+@implementation RNLocalDeal
+
+@end
