b7ae798249d7066f432c7fe8a5adf9c468312f12
Ethan Mick <ethan@cloudmine.me>
2013-05-07 23:59:22 -0400

    Updated view to become larger when more items are added to the Cart.

--- RewardsNow/RNCartViewController.h
+++ RewardsNow/RNCartViewController.h
@@ -15,6 +15,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *pointsLeftLabel;
 @property (weak, nonatomic) IBOutlet UILabel *topPointsLabel;
 @property (weak, nonatomic) IBOutlet UIButton *checkoutButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableHeight;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *innerView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *innerViewHeight;
 
 - (IBAction)confirmTapped:(id)sender;
 - (IBAction)cancelTapped:(id)sender;
--- RewardsNow/RNCartViewController.m
+++ RewardsNow/RNCartViewController.m
@@ -32,7 +32,7 @@
     self.pointsAvailableLabel.text = [_cart.user stringBalance];
     self.pointsInCartLabel.text = [_cart stringTotal];
     self.pointsLeftLabel.text = [_cart stringPointsDifference];
-
+    [self resizeView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -40,9 +40,23 @@
     self.checkoutButton.enabled = _cart.items.count > 0;
 }
 
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    self.scrollView.contentOffset = CGPointZero;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
  
+}
+
+- (void)resizeView {
+    
+    CGFloat difference = self.innerViewHeight.constant - self.tableHeight.constant;
+    self.tableHeight.constant = self.tableView.rowHeight * _cart.items.count;
+    self.innerViewHeight.constant = self.tableHeight.constant + difference;
+    self.scrollView.contentSize = CGSizeMake(320, self.innerViewHeight.constant);
+    [self.view layoutIfNeeded];
 }
 
 - (IBAction)confirmTapped:(id)sender {
