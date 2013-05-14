0a24e8b80ceb0aa6194749e41374333de6eee7dd
Ethan Mick <ethan@cloudmine.me>
2013-05-14 16:10:58 -0400

    Added NSString additions.

---
+++ RewardsNow/NSString+Additions.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Additions.h
+//  Kermits
+//
+//  Created by Ethan Mick on 4/30/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+- (BOOL)isEmpty;
+- (NSString *)leftPadding;
+- (NSString *)rightPadding;
+
+@end
---
+++ RewardsNow/NSString+Additions.m
@@ -0,0 +1,36 @@
+//
+//  NSString+Additions.m
+//  Kermits
+//
+//  Created by Ethan Mick on 4/30/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+// http://stackoverflow.com/questions/3436173/nsstring-is-empty
+//
+
+#import "NSString+Additions.h"
+
+@implementation NSString (Additions)
+
+- (BOOL)isEmpty {
+    if([self length] == 0) { //string is empty or nil
+        return YES;
+    }
+    
+    if(![[self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length]) {
+        //string is all whitespace
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (NSString *)leftPadding {
+    return [NSString stringWithFormat:@" %@", self];
+}
+
+- (NSString *)rightPadding {
+    return [NSString stringWithFormat:@"%@ ", self];
+}
+
+@end
