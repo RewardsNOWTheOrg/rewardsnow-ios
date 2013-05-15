1859cd57a27219820cf1459c128ac7c0f5c8a870
Ethan Mick <ethan@cloudmine.me>
2013-05-14 20:51:07 -0400

    Created the entire detail view. It's amazing!

--- RewardsNow/RNLocalDetailViewController.h
+++ RewardsNow/RNLocalDetailViewController.h
@@ -9,9 +9,21 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
+@class RNLocalDeal, CMLabel;
+
 @interface RNLocalDetailViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate>
+
+@property (nonatomic, strong) RNLocalDeal *deal;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *topImageView;
+@property (weak, nonatomic) IBOutlet UILabel *upperTopLabel;
+@property (weak, nonatomic) IBOutlet UILabel *upperMiddleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *upperLowerLabel;
+@property (weak, nonatomic) IBOutlet CMLabel *lowerUpperLabel;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet CMLabel *mapAddressLabel;
+@property (weak, nonatomic) IBOutlet UIView *lowerInnerView;
 
 - (IBAction)directionsTapped:(id)sender;
+
 @end
--- RewardsNow/RNLocalDetailViewController.m
+++ RewardsNow/RNLocalDetailViewController.m
@@ -8,8 +8,20 @@
 
 #import "RNLocalDetailViewController.h"
 #import "RNLocalAdditionalViewController.h"
+#import "RNLocalDeal.h"
+#import "UIImageView+AFNetworking.h"
+#import "RNAnnotation.h"
+#import "CMLabel.h"
+#import "NSString+Additions.h"
+#import <QuartzCore/QuartzCore.h>
+#import <AddressBookUI/AddressBookUI.h>
+
+#define MapSpanSize 0.05
 
 @interface RNLocalDetailViewController ()
+
+@property (nonatomic) BOOL hasFinishedLoadingMap;
+@property (nonatomic, copy) NSArray *cellRows;
 
 @end
 
@@ -17,6 +29,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.hasFinishedLoadingMap = NO;
+    
+    self.upperTopLabel.text = _deal.businessName;
+    self.upperMiddleLabel.text = _deal.address;
+    self.upperLowerLabel.text = _deal.name;
+    [self.topImageView setImageWithURL:_deal.imageURL];
+    
+    self.lowerUpperLabel.text = _deal.localDealDescription;
+    self.lowerUpperLabel.layer.cornerRadius = 4.0;
+    self.lowerUpperLabel.textInsets = UIEdgeInsetsMake(2, 4, 2, 4);
+    
+    _mapView.layer.cornerRadius = 4.0;
+    _mapView.layer.masksToBounds = YES;
+    
+    self.mapAddressLabel.text = ABCreateStringWithAddressDictionary(_deal.addressDictionary, NO);
+    
+    
+    NSMutableArray *rows = [NSMutableArray array];
+    
+    if (_deal.website != nil && [_deal.website.absoluteString isNotEmpty]) {
+        [rows addObject:@{@"title": [NSString stringWithFormat:@"Visit: %@", _deal.website.absoluteString], @"action" : ^{
+            if (![[UIApplication sharedApplication] openURL:_deal.website]) {NSLog(@"Failed to open url.");}
+            }
+         }];
+    }
+    
+    if (_deal.phoneNumber != nil) {
+        [rows addObject:@{@"title": [NSString stringWithFormat:@"Call: %@", _deal.phoneNumber], @"action" : ^{
+            if (![[UIApplication sharedApplication] openURL:_deal.phoneURL]) {NSLog(@"Failed to make call");}
+            }   
+         }];
+    }
+    
+    [rows addObject:@{@"title": @"Additional Information", @"action" : ^{
+        RNLocalAdditionalViewController *more = [self.storyboard instantiateViewControllerWithIdentifier:@"RNLocalAdditionalViewController"];
+        [self.navigationController pushViewController:more animated:YES];
+    }
+     }];
+    
+    self.cellRows = rows;
 
 }
 
@@ -25,32 +77,52 @@
 
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    
+//    
+//    UIGraphicsBeginImageContext(self.lowerUpperLabel.frame.size);
+//    [[UIImage imageNamed:@"grey-button.png"] drawInRect:self.lowerUpperLabel.bounds];
+//    UIImage *image2 = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+    self.lowerUpperLabel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"grey-button.png"] ];
+}
+
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    self.scrollView.contentSize = CGSizeMake(320, 700);
-    DLog(@"Sup: %@", NSStringFromCGRect(self.scrollView.frame));
-    DLog(@"Size: %@", NSStringFromCGSize(self.scrollView.contentSize));
+    
+    [self.scrollView setScrollEnabled:YES];
+    CGSize size = _scrollView.contentSize;
+    size.height = _lowerInnerView.frame.origin.y + _lowerInnerView.frame.size.height;
+    [self.scrollView setContentSize:size];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    self.scrollView.contentOffset = CGPointZero;
 }
 
 - (IBAction)directionsTapped:(id)sender {
     
+    MKPlacemark *place = [[MKPlacemark alloc] initWithCoordinate:_deal.coordinate2D addressDictionary:[_deal addressDictionary]];
+    MKMapItem* destination = [[MKMapItem alloc] initWithPlacemark:place];
+    destination.name = _deal.businessName;
+    [MKMapItem openMapsWithItems:@[destination] launchOptions:@{MKLaunchOptionsDirectionsModeDriving: MKLaunchOptionsDirectionsModeKey}];
 }
 
 #pragma mark - UITableView Methods
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return _cellRows.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     NSString *CellIdentifier = @"LocalDealDetailCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    cell.textLabel.text = @[@"Call", @"Visit", @"Additional Information"][indexPath.row];
+    cell.textLabel.text = _cellRows[indexPath.row][@"title"];
     
     return cell;
 }
@@ -58,24 +130,59 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    switch (indexPath.row) {
-        case 0:
-        {
-            //call
-            break;
+    void (^action)(void) = _cellRows[indexPath.row][@"action"];
+    action();
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id < MKAnnotation >)annotation {
+    
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    
+    if ([annotation isKindOfClass:[RNAnnotation class]]) {
+        
+        // try to dequeue an existing pin view first
+        static NSString *AddresssAnnotationIdentifier = @"RNAnnotationIdentifier";
+        
+        MKPinAnnotationView *pinView = (MKPinAnnotationView *) [self.mapView dequeueReusableAnnotationViewWithIdentifier:AddresssAnnotationIdentifier];
+        if (!pinView) {
+            // if an existing pin view was not available, create one
+            MKPinAnnotationView *customPinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:AddresssAnnotationIdentifier];
+            customPinView.pinColor = MKPinAnnotationColorRed;
+            customPinView.animatesDrop = NO;
+            customPinView.canShowCallout = YES;
+            return customPinView;
+        } else {
+            pinView.annotation = annotation;
         }
-        case 1:
-        {
-            /// visit
-            break;
-        }
-        default:
-        {
-            RNLocalAdditionalViewController *more = [self.storyboard instantiateViewControllerWithIdentifier:@"RNLocalAdditionalViewController"];
-            [self.navigationController pushViewController:more animated:YES];
-            break;
-        }
+        
+        return pinView;
     }
+    
+    return nil;
+}
+
+- (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView {
+    
+    if (!_hasFinishedLoadingMap) {
+        [self.mapView setRegion:MKCoordinateRegionMake(_deal.coordinate2D, MKCoordinateSpanMake(MapSpanSize, MapSpanSize)) animated:NO];
+        [self placeAnnotationAtLocation:_deal.coordinate2D];
+    }
+    
+    self.hasFinishedLoadingMap = YES;
+}
+
+- (void)placeAnnotationAtLocation:(CLLocationCoordinate2D)location {
+    
+    RNAnnotation *annotation = [[RNAnnotation alloc] init];
+    annotation.coordinate = location;
+    annotation.title = _deal.businessName;
+    annotation.subtitle = _deal.address;
+    
+    [self.mapView addAnnotation:annotation];
+    [self.mapView selectAnnotation:annotation animated:YES];
 }
 
 
