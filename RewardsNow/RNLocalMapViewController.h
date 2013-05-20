30eb71abe9b41443f3f240c5fed5ccf7d95cd1c6
Ethan Mick <ethan@cloudmine.me>
2013-05-20 15:34:13 -0400

    Finished up map view.

--- RewardsNow/RNLocalMapViewController.h
+++ RewardsNow/RNLocalMapViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface RNLocalMapViewController : UIViewController <MKMapViewDelegate, UISearchDisplayDelegate>
+@interface RNLocalMapViewController : UIViewController <MKMapViewDelegate, UISearchDisplayDelegate, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic, copy) NSArray *deals;
--- RewardsNow/RNLocalMapViewController.m
+++ RewardsNow/RNLocalMapViewController.m
@@ -9,8 +9,14 @@
 #import "RNLocalMapViewController.h"
 #import "RNAnnotation.h"
 #import "RNLocalDeal.h"
+#import "RNConstants.h"
+#import "RNLocalDetailViewController.h"
 
 @interface RNLocalMapViewController ()
+
+@property (nonatomic) BOOL isFirstLoad;
+@property (nonatomic, strong) NSMutableArray *searchedDeals;
+@property (nonatomic, strong) NSMutableArray *annotations;
 
 @end
 
@@ -19,19 +25,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.isFirstLoad = YES;
+    self.searchedDeals = [NSMutableArray array];
+    self.annotations = [NSMutableArray array];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
 
-#pragma mark - Object Methods
+#pragma mark - MKMapView Methods
 
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id < MKAnnotation >)annotation {
     
@@ -50,6 +61,10 @@
             customPinView.pinColor = MKPinAnnotationColorRed;
             customPinView.animatesDrop = NO;
             customPinView.canShowCallout = YES;
+            
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+            [button addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
+            customPinView.rightCalloutAccessoryView = button;
             return customPinView;
         } else {
             pinView.annotation = annotation;
@@ -61,7 +76,58 @@
     return nil;
 }
 
+- (MKCoordinateRegion)calculateRegionForDeals:(NSArray *)deals {
+    
+    if (deals.count == 0) {
+        return MKCoordinateRegionMake(_location, MKCoordinateSpanMake(MapSpanSize, MapSpanSize));
+    }
+    
+    double minLat = INFINITY;
+    double minLong = INFINITY;
+    double maxLat = -INFINITY;
+    double maxLong = -INFINITY;
+    double latitude = 0;
+    double longitude = 0;
+    
+    for (NSInteger i = 0; i < deals.count; i++) {
+        RNLocalDeal *loc = deals[i];
+        
+        latitude += loc.location.coordinate.latitude;
+        longitude += loc.location.coordinate.longitude;
+        
+        if (loc.location.coordinate.latitude < minLat) {
+            minLat = loc.location.coordinate.latitude;
+        }
+        
+        if (loc.location.coordinate.latitude > maxLat) {
+            maxLat = loc.location.coordinate.latitude;
+        }
+        
+        if (loc.location.coordinate.longitude < minLong) {
+            minLong = loc.location.coordinate.longitude;
+        }
+        
+        if (loc.location.coordinate.longitude > maxLong) {
+            maxLong = loc.location.coordinate.longitude;
+        }
+    }
+    
+    latitude /= deals.count;
+    longitude /= deals.count;
+    
+    double spanLat = (maxLat - minLat) * 1.5 < MapSpanSize ? MapSpanSize : (maxLat - minLat) * 1.5;
+    double spanLong = (maxLong - minLong) * 1.5 < MapSpanSize ? MapSpanSize : (maxLong - minLong) * 1.5;
+    
+    return MKCoordinateRegionMake(CLLocationCoordinate2DMake(latitude, longitude), MKCoordinateSpanMake( spanLat, spanLong));
+}
+
 - (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView {
+    
+    if (_isFirstLoad) {
+        self.isFirstLoad = NO;
+        [self.mapView setRegion:[self calculateRegionForDeals:_deals] animated:YES];
+    }
+
     
     for (NSInteger i = 0; i < _deals.count; i++) {
         RNLocalDeal *deal = _deals[i];
@@ -79,7 +145,9 @@
     annotation.coordinate = location;
     annotation.title = deal.businessName;
     annotation.subtitle = deal.address;
+    annotation.deal = deal;
     
+    [self.annotations addObject:annotation];
     [self.mapView addAnnotation:annotation];
 }
 
@@ -96,6 +164,58 @@
         MKPinAnnotationView *theView = (MKPinAnnotationView *)view;
         theView.pinColor = MKPinAnnotationColorRed;
     }
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+
+    RNLocalDetailViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"RNLocalDetailViewController"];
+    controller.deal = ((RNAnnotation *)view.annotation).deal;
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+#pragma mark - UISearchBar Table
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    
+    DLog(@"Search: %@", searchText);
+    
+    [self filterWithQuery:searchText];
+}
+
+- (void)filterWithQuery:(NSString *)query {
+    [_searchedDeals removeAllObjects];
+    for (RNLocalDeal *deal in _deals) {
+        if ([deal doesMatchQuery:query]) {
+            [_searchedDeals addObject:deal];
+        }
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _searchedDeals.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"SearchDisplayCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [_searchedDeals[indexPath.row] businessName];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self.searchDisplayController setActive:NO animated:YES];
+    
+    RNLocalDeal *deal = _searchedDeals[indexPath.row];
+    NSUInteger index = [_deals indexOfObject:deal];
+    [self.mapView selectAnnotation:_annotations[index] animated:YES];
+    [self.mapView setRegion:[self calculateRegionForDeals:@[deal]] animated:YES];
 }
 
 @end
