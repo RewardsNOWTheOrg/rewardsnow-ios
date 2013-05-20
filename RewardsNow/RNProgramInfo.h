d7292f7c3a6642c41186fc4be7d38f0ca0e1404a
Ethan Mick <ethan@cloudmine.me>
2013-05-20 15:52:30 -0400

    Added in a file to hold the program info.

---
+++ RewardsNow/RNProgramInfo.h
@@ -0,0 +1,13 @@
+//
+//  RNProgramInfo.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/20/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNObject.h"
+
+@interface RNProgramInfo : RNObject
+
+@end
---
+++ RewardsNow/RNProgramInfo.m
@@ -0,0 +1,13 @@
+//
+//  RNProgramInfo.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/20/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNProgramInfo.h"
+
+@implementation RNProgramInfo
+
+@end
