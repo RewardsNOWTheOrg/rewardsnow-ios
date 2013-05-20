10c668e65bc58e4ebf19057c91aa6a97f7aabf2e
Ethan Mick <ethan@cloudmine.me>
2013-05-20 11:43:53 -0400

    Added the filerview.

---
+++ RewardsNow/RNLocalFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNLocalFilterViewController.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/20/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNLocalFilterViewController : UIViewController
+
+@end
---
+++ RewardsNow/RNLocalFilterViewController.m
@@ -0,0 +1,27 @@
+//
+//  RNLocalFilterViewController.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 5/20/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNLocalFilterViewController.h"
+
+@interface RNLocalFilterViewController ()
+
+@end
+
+@implementation RNLocalFilterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+@end
