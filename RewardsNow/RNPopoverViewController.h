4ad72c59555b4685b1c6f5650efd2a4666b21868
Ethan Mick <ethan@cloudmine.me>
2013-06-07 17:27:04 -0400

    Added a popup controller.

---
+++ RewardsNow/RNPopoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNPopoverViewController.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 6/7/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNPopoverViewController : UIViewController
+
+@end
---
+++ RewardsNow/RNPopoverViewController.m
@@ -0,0 +1,27 @@
+//
+//  RNPopoverViewController.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 6/7/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNPopoverViewController.h"
+
+@interface RNPopoverViewController ()
+
+@end
+
+@implementation RNPopoverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+@end
