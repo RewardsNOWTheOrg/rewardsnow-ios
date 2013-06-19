4ff8c68d57a6a90db2b04b7a89ea673c15820afd
Ethan Mick <ethanmski@gmail.com>
2013-06-19 15:13:50 -0400

    Added in the right arrow button.

---
+++ RewardsNow/RNRightArrow.h
@@ -0,0 +1,13 @@
+//
+//  RNRightArrow.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 6/19/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNLeftArrow.h"
+
+@interface RNRightArrow : RNLeftArrow
+
+@end
---
+++ RewardsNow/RNRightArrow.m
@@ -0,0 +1,31 @@
+//
+//  RNRightArrow.m
+//  RewardsNow
+//
+//  Created by Ethan Mick on 6/19/13.
+//  Copyright (c) 2013 CloudMine LLC. All rights reserved.
+//
+
+#import "RNRightArrow.h"
+
+@implementation RNRightArrow
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
