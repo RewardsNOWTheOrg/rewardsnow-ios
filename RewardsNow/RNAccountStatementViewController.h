3c426df1cf2995f0efff228ab21a57072a4d5296
Ethan Mick <ethanmski@gmail.com>
2013-07-03 16:09:51 -0400

    Updated the account statement page to universal acclaim from Nic and Emilia, removed swiping and made it all one nice UITableview.

--- RewardsNow/RNAccountStatementViewController.h
+++ RewardsNow/RNAccountStatementViewController.h
@@ -14,9 +14,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *pointsStartLabel;
 @property (weak, nonatomic) IBOutlet UILabel *pointsEndLabel;
 @property (weak, nonatomic) IBOutlet UILabel *monthLabel;
-@property (weak, nonatomic) IBOutlet UIView *innerView;
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIView *displayedDetailView;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *backMonthButton;
 @property (weak, nonatomic) IBOutlet UIButton *forwardMonthButton;
@@ -24,6 +21,5 @@
 
 - (IBAction)backMonth:(id)sender;
 - (IBAction)forwardMonth:(id)sender;
-- (IBAction)swipeRecognized:(id)sender;
 
 @end
--- RewardsNow/RNAccountStatementViewController.m
+++ RewardsNow/RNAccountStatementViewController.m
@@ -20,14 +20,6 @@
 
 #define kNumberOfMonthsVisible 4
 
-typedef enum {
-    DetailViewTable = 0,
-    DetailViewIncrease = 1,
-    DetailViewDecrease,
-    DetailViewHistory,
-    DetailViewOther
-} DetailView;
-
 static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
 
 @interface RNAccountStatementViewController ()
@@ -37,13 +29,16 @@ static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
 @property (nonatomic, strong) NSDate *firstDay;
 @property (nonatomic, strong) NSDate *lastDay;
 @property (nonatomic, strong) RNAccountStatement *statement;
-@property (nonatomic) DetailView currentState;
 @property (nonatomic, strong) NSDate *finalLastDay;
 @property (nonatomic, strong) NSDate *finalFirstDay;
+
+@property (nonatomic, copy) NSArray *internalStatement;
 
 @end
 
 #define kTableY 80
+#define kTableHeaderHeight 30
+#define kSectionTitles @[@"Points Increased", @"Points Decreased", @"Activity Detail"]
 
 @implementation RNAccountStatementViewController
 
@@ -65,22 +60,7 @@ static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
         self.finalFirstDay = [_finalFirstDay firstDayOfPreviousMonth];
     }
     
-    self.currentState = DetailViewTable;
-    
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, kTableY, 320, 44*4) style:UITableViewStylePlain];
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:RNAccountStatementCell];
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
-    self.tableView.scrollEnabled = NO;
-    [self.innerView addSubview:self.tableView];
-    self.displayedDetailView = self.tableView;
-    
     [self updateInfo];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-
 }
 
 #pragma mark - UIActions
@@ -108,53 +88,6 @@ static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
     self.backMonthButton.enabled = ![self.firstDay isEqualToDate:self.finalFirstDay];
 }
 
-- (IBAction)swipeRecognized:(UISwipeGestureRecognizer *)sender {
-    
-    DLog(@"Direction: %d", sender.direction);
-    NSInteger change = sender.direction == UISwipeGestureRecognizerDirectionLeft ? 1 : -1;
-    
-    DLog(@"State: %d", _currentState);
-    
-    if (_currentState == 0 && change == -1) {
-        _currentState = 4;
-    } else if (_currentState == 4 && change == 1) {
-        _currentState = 0;
-    } else {
-        _currentState += change;
-    }
-    
-    switch (self.currentState) {
-        case DetailViewTable:
-        {
-            [self showTable:sender.direction];
-            break;
-        }
-        case DetailViewIncrease:
-        {
-            [self showPointsIncrease:sender.direction];
-            break;
-        }
-        case DetailViewDecrease:
-        {
-            [self showPointsDecrease:sender.direction];
-            break;
-        }
-        case DetailViewHistory:
-        {
-            [self showActivityDetail:sender.direction];
-            break;
-        }
-        case DetailViewOther:
-        {
-            [self showOther:sender.direction];
-            break;
-        }
-        default:
-            DLog(@"Unexpected Value: %d", _currentState);
-            break;
-    }
-}
-
 - (void)updateInfo {
     [self updateControls];
     
@@ -171,8 +104,12 @@ static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
         
         if ([response wasSuccessful]) {
             self.statement = response.result;
+            
+            self.internalStatement = @[_statement.pointsIncrease, _statement.pointsDecrease, _statement.history];
+            
             self.pointsStartLabel.text = [self.statement.pointsBeginning description] == nil ? @"0" : [self.statement.pointsBeginning description];
             self.pointsEndLabel.text = [self.statement.pointsEnd description] == nil ? @"0" : [self.statement.pointsEnd description];
+            [self.tableView reloadData];
         } else {
             [[[UIAlertView alloc] initWithTitle:@"Error" message:response.errorString delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil] show];
         }
@@ -182,132 +119,66 @@ static NSString *RNAccountStatementCell = @"RNAccountStatementCell";
 
 #pragma mark - UITableView Datasource
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return _internalStatement.count;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 4;
+    NSInteger rows = [_internalStatement[section] count];
+    if (rows == 0) {
+        rows = 1;
+    }
+    return rows;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:RNAccountStatementCell];
     
-    cell.textLabel.text = @[@"Points Increased", @"Points Decreased", @"Activity Detail", @"Other"][indexPath.row];
-    cell.textLabel.font = [UIFont fontWithName:@"Arial" size:14];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.font = [UIFont fontWithName:@"Arial" size:17];
+    cell.detailTextLabel.textColor = [UIColor blackColor];
+    
+    NSString *titleText;
+    NSString *detailText;
+    
+    if ([_internalStatement[indexPath.section] count] > 0) {
+        if (indexPath.section == 2) {
+            RNTransaction *trans = _internalStatement[indexPath.section][indexPath.row];
+            titleText = trans.transactionDescription;
+            detailText = [trans formattedStringFromNumber:trans.points];
+        } else {
+            RNPointChange *pointChange = _internalStatement[indexPath.section][indexPath.row];
+            titleText = pointChange.statementType;
+            detailText = [pointChange formattedStringFromNumber:pointChange.points];
+        }
+    } else {
+        titleText = @"None";
+    }
+    
+    cell.textLabel.text = titleText;
+    cell.detailTextLabel.text = detailText;
+    
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    cell.backgroundColor = self.branding.backgroundColor;
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UIView *headerSection = [[UIView alloc] init];
+    
+    CMLabel *title = [[CMLabel alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, kTableHeaderHeight)];
+    title.backgroundColor = self.branding.pointsColor;
+    title.text = kSectionTitles[section];
+    [headerSection addSubview:title];
+    
+    return headerSection;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    switch (indexPath.row) {
-        case 0:
-        {
-            [self showPointsIncrease:UISwipeGestureRecognizerDirectionLeft];
-            break;
-        }
-        case 1:
-        {
-            [self showPointsDecrease:UISwipeGestureRecognizerDirectionLeft];
-            break;
-        }
-        case 2:
-        {
-            [self showActivityDetail:UISwipeGestureRecognizerDirectionLeft];
-            break;
-        }
-        case 3:
-        {
-            [self showOther:UISwipeGestureRecognizerDirectionLeft];
-            break;
-        }
-        default:
-            break;
-    }
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
+    view.backgroundColor = self.branding.pointsColor;
 }
 
-#pragma mark - Swiping Methods
-
-- (void)showTable:(UISwipeGestureRecognizerDirection)direction {
-    
-    UITableView *aTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 121, 320, 44*4) style:UITableViewStylePlain];
-    [aTable registerClass:[UITableViewCell class] forCellReuseIdentifier:RNAccountStatementCell];
-    aTable.delegate = self;
-    aTable.dataSource = self;
-    aTable.scrollEnabled = NO;
-    
-    [self scrollDetailView:direction toView:aTable];
-}
-
-- (void)showPointsIncrease:(UISwipeGestureRecognizerDirection)direction {
-    
-    RNAccountStatementDetailView *details = [[RNAccountStatementDetailView alloc] initWithFrame:CGRectMake(10, kTableY, 300, (self.statement.pointsIncrease.count * 40) + 70)];
-    details.titleLabel.text = @"Points Increase";
-
-    DLog(@"Info: %@", self.statement);
-    
-    for (NSInteger i = 0; i < self.statement.pointsIncrease.count; i++) {
-        RNPointChange *pc = self.statement.pointsIncrease[i];
-        [details addLeftText:pc.statementType rightText:[pc.points description]];
-    }
-    
-    [self scrollDetailView:direction toView:details];
-}
-
-- (void)showPointsDecrease:(UISwipeGestureRecognizerDirection)direction {
-    
-    RNAccountStatementDetailView *details = [[RNAccountStatementDetailView alloc] initWithFrame:CGRectMake(10, kTableY, 300, (self.statement.pointsDecrease.count * 40) + 70)];
-    details.titleLabel.text = @"Points Decrease";
-    
-    for (NSInteger i = 0; i < self.statement.pointsDecrease.count; i++) {
-        RNPointChange *pc = self.statement.pointsDecrease[i];
-        [details addLeftText:pc.statementType rightText:[pc.points description]];
-    }
-    
-    [self scrollDetailView:direction toView:details];
-    
-}
-
-- (void)showActivityDetail:(UISwipeGestureRecognizerDirection)direction {
-    RNAccountStatementDetailView *details = [[RNAccountStatementDetailView alloc] initWithFrame:CGRectMake(10, kTableY, 300, (self.statement.history.count * 40) + 70)];
-    details.titleLabel.text = @"Activity Detail";
-    
-    for (NSInteger i = 0; i < self.statement.history.count; i++) {
-        RNTransaction *transaction = self.statement.history[i];
-        [details addLeftText:transaction.transactionDescription rightText:[transaction.points description]];
-    }
-    
-    [self scrollDetailView:direction toView:details];
-}
-
-- (void)showOther:(UISwipeGestureRecognizerDirection)direction {
-    
-    RNAccountStatementDetailView *details = [[RNAccountStatementDetailView alloc] initWithFrame:CGRectMake(10, kTableY, 300, 300)];
-    details.titleLabel.text = @"Other";
-
-    [self scrollDetailView:direction toView:details];
-}
-
-- (void)scrollDetailView:(UISwipeGestureRecognizerDirection)direction toView:(UIView *)aView {
-    
-    aView.backgroundColor = self.branding.backgroundColor;
-    
-    CGFloat modifier = direction == UISwipeGestureRecognizerDirectionLeft ? 320 : -320;
-    
-    aView.frame = CGRectMake(aView.frame.origin.x + modifier, aView.frame.origin.y, aView.frame.size.width, aView.frame.size.height);
-    [self.innerView addSubview:aView];
-    
-    [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        
-        self.displayedDetailView.frame = CGRectMake(-(modifier), self.displayedDetailView.frame.origin.y, self.displayedDetailView.frame.size.width, self.displayedDetailView.frame.size.height);
-        aView.frame = CGRectMake(aView.frame.origin.x - (modifier), aView.frame.origin.y, aView.frame.size.width, aView.frame.size.height);
-        
-    } completion:^(BOOL finished) {
-        self.displayedDetailView = aView;
-        self.scrollView.contentSize = CGSizeMake(320, self.displayedDetailView.frame.origin.y + self.displayedDetailView.frame.size.height);
-    }];
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return kTableHeaderHeight;
 }
 
 #pragma mark - Helper Method
