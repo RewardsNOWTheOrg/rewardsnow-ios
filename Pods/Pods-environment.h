cdfac435cc92b9accd5c0999f72bff413f164011
Ethan Mick <ethanmski@gmail.com>
2013-10-02 17:40:56 -0400

    Version 1.0

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -15,36 +15,18 @@
 // MBProgressHUD
 #define COCOAPODS_POD_AVAILABLE_MBProgressHUD
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
-#define COCOAPODS_VERSION_MINOR_MBProgressHUD 7
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
 // Mantle
 #define COCOAPODS_POD_AVAILABLE_Mantle
 #define COCOAPODS_VERSION_MAJOR_Mantle 1
-#define COCOAPODS_VERSION_MINOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 3
 #define COCOAPODS_VERSION_PATCH_Mantle 0
 
-// SparkInspector
-#define COCOAPODS_POD_AVAILABLE_SparkInspector
-#define COCOAPODS_VERSION_MAJOR_SparkInspector 1
-#define COCOAPODS_VERSION_MINOR_SparkInspector 0
-#define COCOAPODS_VERSION_PATCH_SparkInspector 10
-
-// libextobjc/EXTKeyPathCoding
-#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTKeyPathCoding
-#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTKeyPathCoding 0
-#define COCOAPODS_VERSION_MINOR_libextobjc_EXTKeyPathCoding 2
-#define COCOAPODS_VERSION_PATCH_libextobjc_EXTKeyPathCoding 5
-
-// libextobjc/EXTScope
-#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTScope
-#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTScope 0
-#define COCOAPODS_VERSION_MINOR_libextobjc_EXTScope 2
-#define COCOAPODS_VERSION_PATCH_libextobjc_EXTScope 5
-
-// libextobjc/RuntimeExtensions
-#define COCOAPODS_POD_AVAILABLE_libextobjc_RuntimeExtensions
-#define COCOAPODS_VERSION_MAJOR_libextobjc_RuntimeExtensions 0
-#define COCOAPODS_VERSION_MINOR_libextobjc_RuntimeExtensions 2
-#define COCOAPODS_VERSION_PATCH_libextobjc_RuntimeExtensions 5
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 3
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 0
 
