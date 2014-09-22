1ae6aaae39a531833c95f0de04eb9b58c8f32868
Ethan Mick <ethanmski@gmail.com>
2014-09-22 11:37:44 -0400

    Added in RNLabel to replace CMLabel.

---
+++ RewardsNow/RNLabel.h
@@ -0,0 +1,15 @@
+//
+//  RNLabel.h
+//  RewardsNow
+//
+//  Created by Ethan Mick on 9/22/14.
+//  Copyright (c) 2014 CloudMine LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNLabel : UILabel
+
+@property (nonatomic) UIEdgeInsets textInsets;
+
+@end
