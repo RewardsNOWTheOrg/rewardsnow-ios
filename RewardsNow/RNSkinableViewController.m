86376e95b533aa7f7d037022f1d7daf3ab4ca24c
Ethan Mick <ethanmski@gmail.com>
2014-09-22 11:31:01 -0400

    Updated storyboard for iOS 8.

--- RewardsNow/RNSkinableViewController.m
+++ RewardsNow/RNSkinableViewController.m
@@ -27,8 +27,10 @@
     
     self.view.backgroundColor = _branding.commonBackgroundColor;
     self.navigationController.navigationBar.tintColor = _branding.menuBackgroundColor;
-    [self.tabBarController.tabBarItem setTitleTextAttributes:@{UITextAttributeTextColor: _branding.tabBarTextColor} forState:UIControlStateNormal];
-    [self.tabBarController.tabBarItem setTitleTextAttributes:@{UITextAttributeTextColor: _branding.tabBarTextColor} forState:UIControlStateNormal];
+    [self.tabBarController.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: _branding.tabBarTextColor}
+                                                    forState:UIControlStateNormal];
+    [self.tabBarController.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: _branding.tabBarTextColor}
+                                                    forState:UIControlStateNormal];
     
     if ([self respondsToSelector:@selector(innerView)]) {
         [[self performSelector:@selector(innerView)] setBackgroundColor:_branding.commonBackgroundColor];
